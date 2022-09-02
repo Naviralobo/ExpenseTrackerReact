e740b4435f04a0abc39546eb1470adbaf9d3f202
Naviralobo <110480237+Naviralobo@users.noreply.github.com>
2022-09-02 18:02:57 +0530

    FirstCommit

---
+++ ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <div>
+          <h2>Expense Item!</h2>
+          <p>food 10</p>
+          <p>petrol 100</p>
+          <p>movie 200</p>
+          <p>Snack 10</p>
+        </div>
+      );
+}
+
+export default ExpenseItem;
