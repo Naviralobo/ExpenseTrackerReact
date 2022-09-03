3689bb138b4de5493e3e0a5e07ac1c0309e66c14
Naviralobo <110480237+Naviralobo@users.noreply.github.com>
2022-09-03 16:57:39 +0530

    looping

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure: "Uno Super Market",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: "Samsung Showroom",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: "Gpay",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      locationOfExpenditure: "Sanchu Furniture",
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <h2>Let's get started </h2>
+      {expenses.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+          location={item.locationOfExpenditure}
+        ></ExpenseItem>
+      ))}
     </div>
   );
 }
